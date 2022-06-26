87166364935b1d939db9a06379b84c3a945b42d9
Xiangwei Tang <tangxiangwei@gmail.com>
2022-06-26 17:51:10 +0800

    Add project files.

---
+++ AfterScanPrintScreen/MainWorkflow.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
+
+namespace AfterScanPrintScreen
+{
+    internal class MainWorkflow
+    {
+        public MainWorkflow()
+        {
+
+        }
+        public void Run()
+        {
+            CallScanWindow();
+        }
+        ///
+        /// The full url document can be found here: https://docs.microsoft.com/en-us/windows/uwp/launch-resume/launch-settings-app#ms-settings-uri-scheme-reference
+        /// 
+        /// <summary>
+        /// Call the virus and threat protection window.
+        /// </summary>
+        private void CallScanWindow()
+        {
+            Process.Start("cmd.exe", $"/Cstart ms-settings:windowsdefender");
+            // Sleep 5 seconds for the delay of window display.
+            Thread.Sleep(5000);
+            PressKeyHelper.PressSpecialKey("tab");
+            PressKeyHelper.PressSpecialKey("enter");
+        }
+    }
+}
---
+++ AfterScanPrintScreen/PressKeyHelper.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace AfterScanPrintScreen
+{
+    internal static class PressKeyHelper
+    {
+        ///
+        /// The full key-code mapping table is in "Data\KeyCode.txt".
+        ///
+
+        /// <summary>
+        /// Call the external functions.
+        /// </summary>
+        /// <param name="bVk"></param>
+        /// <param name="bScan"></param>
+        /// <param name="dwFlags"></param>
+        /// <param name="dwExtraInfo"></param>
+        [DllImport("user32.dll")]
+        static extern void keybd_event(byte bVk, byte bScan, int dwFlags, int dwExtraInfo);
+        private const int KEY_DOWN_FLAG = 0x0001;
+        private const int KEY_UP_FLAG = 0x0002;
+        public static void PressSpecialKey(string key)
+        {
+            PressKey(SpecialKeyBytes[key]);
+        }
+        public static void PressKey(char letterChar)
+        {
+            if (('A' <= letterChar && letterChar <= 'Z') ||
+                ('0' <= letterChar && letterChar <= '9'))
+            {
+                PressKey((byte)letterChar);
+                return;
+            }
+            if ('a' <= letterChar && letterChar <= 'z')
+            {
+                PressKey((byte)(letterChar - 'a' + 'A'));
+                return;
+            }
+
+            throw new Exception("Non-letter char.");
+        }
+        public static void PressKey(byte b)
+        {
+            KeyDown(b);
+            KeyUp(b);
+        }
+        public static void KeyDown(byte b)
+        {
+            keybd_event(b, 0, KEY_DOWN_FLAG, 0);
+        }
+        public static void KeyUp(byte b)
+        {
+            keybd_event(b, 0, KEY_UP_FLAG, 0);
+        }
+        private static Dictionary<string, byte> SpecialKeyBytes = new Dictionary<string, byte>
+        {
+            {"left_win",0 },
+            {"tab",9 },
+            {"enter",13 },
+            {"left",37 },
+            {"up",38 },
+            {"right",39 },
+            {"down",40 },            
+        };
+    }
+}
